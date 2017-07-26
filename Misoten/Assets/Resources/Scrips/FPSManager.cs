3add232f8796193059d8e0f44a893656714b9936
DESKTOP-05PR4TA\Miyazawa.Fuki <tyukajin2014@gmail.com>
2017-07-26 18:51:17 +0900

    これが真の初期プロジェクトだ・・・

---
+++ Misoten/Assets/Resources/Scrips/FPSManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSManager : MonoBehaviour {
+
+    private static FPSManager instance;
+
+    public bool idDebug;
+
+    private int countFPS;//fpsのカウンタ
+    private float countRunTime;//ゲームの実行時間のカウンタ
+
+    public Text textObject;//FPS表示用オブジェクト
+
+    // Use this for initialization
+    void Start () {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        countFPS = 0;
+        countRunTime = 0.0f;
+
+        //QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+
+        DontDestroyOnLoad(gameObject);
+
+        gameObject.SetActive(idDebug);
+
+        instance = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        countFPS++;
+        float time = Time.realtimeSinceStartup - countRunTime;
+
+        if(time >= 0.5f)
+        {
+            textObject.text = "FPS："+(countFPS/time).ToString("f2");
+
+            countFPS = 0;
+            countRunTime = Time.realtimeSinceStartup;
+        }
+    }
+}
